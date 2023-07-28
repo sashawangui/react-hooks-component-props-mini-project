c033df3f8d449eaad66fbaea6e52769e59db0ad0
sasha wangui <sasha.iluku@gmail.com>
2023-07-28 21:22:01 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import blogData from "../data/blog";
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
+      <ArticleList posts={blogData.posts}/>
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
     </div>
